f4e746bd21dac3fd595f7f846bf37c4b0881d1ff
Anna Lazarenko <oleglazarenko@yandex.ru>
2023-02-09 22:48:51 +0700

    feat: automatic deploy

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-=&eddcbk)=paz@z+1gdi5$#3hy7juryubreultyb6$$!)b3bxg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
